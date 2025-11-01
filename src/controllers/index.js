836993bdae9dafd89b371b7011be4c5888452a5c
La Đại Lộc <ladailoc9a3@gmail.com>
2025-11-01 23:07:53 +0700

    initial project

---
+++ src/controllers/index.js
@@ -0,0 +1,4 @@
+// Export all controllers
+export { default as AuthController } from "./auth.controller.js";
+export { default as UserController } from "./user.controller.js";
+export { default as ProductController } from "./product.controller.js";
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./auth.route.js";
+import userRoutes from "./user.route.js";
+import productRoutes from "./product.route.js";
+import systemRoutes from "./system.route.js";
+
+const router = express.Router();
+
+// Mount routes
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+router.use("/", systemRoutes);
+
+export default router;
